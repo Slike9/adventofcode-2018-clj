86d7f79e4aba5fa5625e45c8d3fff425e506b6ea
Sergey Semyonov <semenov@restream.rt.ru>
2018-12-06 13:33:01 +0400

    Refactor Day 1
    * Extract first-duplicate method.
    * Extract cumulative-sums method.
    * Rename variables.

--- src/adventofcode_2018/day1.clj
+++ src/adventofcode_2018/day1.clj
@@ -2,6 +2,8 @@
   (:gen-class)
   (:require [clojure.java.io :as jio]))
 
+; Utils
+
 (defn read-input
   [file-path]
   (with-open [r (jio/reader file-path)]
@@ -9,31 +11,42 @@
          line-seq
          (mapv #(Integer/parseInt %)))))
 
+(defn cumulative-sums
+  ([coll] (cumulative-sums coll 0))
+  ([coll init]
+   (if (empty? coll)
+     init
+     (lazy-seq
+       (let [[x & more] coll]
+         (cons init (cumulative-sums more (+ init x))))))))
+
+(defn first-duplicate
+  ([coll] (first-duplicate coll #{}))
+  ([coll met]
+   (if (empty? coll)
+     nil
+     (let [[x & more] coll]
+       (if (met x)
+         x
+         (recur more (conj met x)))))))
+
+; Puzzles
+
 (defn resulting-freq
   [freq-changes]
   (reduce + freq-changes))
 
-(defn resulting-freqs
-  ([freq-changes]
-   (resulting-freqs freq-changes 0))
-  ([freq-changes freq]
-   (lazy-seq
-     (let [[freq-change & rest-freq-changes] freq-changes
-           next-freq (+ freq freq-change)]
-       (cons freq (resulting-freqs rest-freq-changes next-freq))))))
-
 (defn first-repeating-resulting-freq
   [freq-changes]
-  (loop [freqs (resulting-freqs (cycle freq-changes))
-         met-freqs #{}]
-    (let [[freq & next-freqs] freqs]
-      (if (met-freqs freq)
-       freq
-       (recur next-freqs (conj met-freqs freq))))))
+  (->> freq-changes
+       cycle
+       cumulative-sums
+       first-duplicate))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (println "--- Day 1: Chronal Calibration ---")
   (let [input (read-input "inputs/day1.txt")]
     (println "Resulting frequency:" (resulting-freq input))
     (println "First repeating resulting frequency:" (first-repeating-resulting-freq input))))
